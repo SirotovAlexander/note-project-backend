181c321ab2e10f50d21e4ec8e06195ce298cee64
SirotovAlexander <alexander.sirotov@gmail.com>
2023-11-08 01:19:40 +0200

    prepare for next step
    prepare for next step

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+
+const app = express();
+
+const { contactRouter, diaryRouter } = require("./routes/api/index");
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contact", contactRouter);
+app.use("/api/diary", diaryRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((error, req, res, next) => {
+  const { status = 500, message = "Server error" } = error;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const contactRouter = require("./contacts");
+const diaryRouter = require("./diary");
+
+module.exports = {
+  contactRouter,
+  diaryRouter,
+};
