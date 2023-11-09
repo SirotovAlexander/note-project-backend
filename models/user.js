a89b1142463a81f6319087655e8d3e06d5a07159
SirotovAlexander <alexander.sirotov@gmail.com>
2023-11-09 23:47:14 +0200

    add validate and chemas
    add validate and chemas

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: [emailRegexp, "Email must be valid"],
+      unique: true,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      minlength: [6, `Password must be at least six characters long`],
+      required: [true, "Password is required"],
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
