a9ce9f0963b2fd045363fea26d0844b4fc6b2ac9
SirotovAlexander <alexander.sirotov@gmail.com>
2023-11-10 00:44:07 +0200

    fix
    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,15 +1,18 @@
 const { HttpError } = require("../helpers/index");
 
 const validateBody = (schema) => {
-  const func = (req, res, next) => {
+  return (req, res, next) => {
+    // Validate the request body using the provided schema
     const { error } = schema.validate(req.body);
+
+    // If validation fails, send a 400 Bad Request response with the validation error message
     if (error) {
       next(HttpError(400, error.message));
     }
+
+    // Continue with the next middleware or route handler if validation succeeds
     next();
   };
-
-  return func;
 };
 
 module.exports = validateBody;
