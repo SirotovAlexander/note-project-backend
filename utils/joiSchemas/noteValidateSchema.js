6cacf4258c3b9bfc65e259906c8c2afb0bd9897f
SirotovAlexander <alexander.sirotov@gmail.com>
2024-01-07 17:53:40 +0200

    Update noteValidateSchema.js

--- utils/joiSchemas/noteValidateSchema.js
+++ utils/joiSchemas/noteValidateSchema.js
@@ -1,11 +1,7 @@
 const Joi = require("joi");
 
-// const dateRegexp =
-//   /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/g;
-
 const noteValidationSchema = Joi.object({
   title: Joi.string().empty().required(),
-  // date: Joi.string().empty().trim().pattern(dateRegexp).required(),
   date: Joi.string().empty().trim().required(),
   text: Joi.string().required(),
 });
